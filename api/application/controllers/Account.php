9839779a016644e1b55a9351554a2e3580c782f8
randiekas <randiekas@gmail.com>
2017-02-15 21:46:55 +0700

    API
    API untuk android nya, baru ada api untuk login saja

---
+++ api/application/controllers/Account.php
@@ -0,0 +1,56 @@
+
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . '/libraries/REST_Controller.php';
+
+// use namespace
+use Restserver\Libraries\REST_Controller;
+
+/**
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array
+ *
+ * @package         CodeIgniter
+ * @subpackage      Rest Server
+ * @category        Controller
+ * @author          Phil Sturgeon, Chris Kacerguis
+ * @license         MIT
+ * @link            https://github.com/chriskacerguis/codeigniter-restserver
+ */
+class Account extends REST_Controller {
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+
+        // Configure limits on our controller methods
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+        //$this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
+        //$this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
+        //$this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
+    }
+
+    public function signin_get()
+    {
+        // Users from a data store e.g. database
+        $response["status"]= "success";
+        $response["message"]= "Login Berhasil";
+        $response["email"]= "";
+        $response["foto"]= "http://scientic.sakolah.com/assets/pegawai/197602192005071001/197602192005071001.jpg";
+        $response["gender"]= "L";
+        $response["id"]= "15";
+        $response["image"]= null;
+        $response["name"]= "Yudi Subekti";
+        $response["nomor_induk"]= "197602192005071001 datang";
+        $response["password"]= "bismilah";
+        $response["school"]= "smkn11bdg";
+        $response["tingkat"]= null;
+        $response["type"]= 2;
+        $this->set_response($response, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+    }
+}
