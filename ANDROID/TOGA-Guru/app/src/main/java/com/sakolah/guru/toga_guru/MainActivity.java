3badf59d83815b4921afc39260b75399a8128624
randiekas <randiekas@gmail.com>
2017-02-28 23:55:02 +0700

    Revert "Revert "CORE SYSTEM ANDROID""
    This reverts commit 610e2c04760abd88beba1f8fd8cfd2e5e711ad09.

---
+++ ANDROID/TOGA-Guru/app/src/main/java/com/sakolah/guru/toga_guru/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sakolah.guru.toga_guru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
