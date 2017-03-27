61d03c8402fe605e6273572a89c77f84263c17c7
revifahreza <revifahreza46@gmail.com>
2017-03-27 21:54:38 +0700

    saya sudah menyelesaikan masalah mengenai listview yang sebelumnya bermasalah

--- ANDROID/TOGA-Guru/app/src/main/java/com/sakolah/guru/toga_guru/NilaiSiswa.java
+++ ANDROID/TOGA-Guru/app/src/main/java/com/sakolah/guru/toga_guru/NilaiSiswa.java
@@ -28,4 +28,29 @@ public class NilaiSiswa extends AppCompatActivity {
         });
 
     }
+	public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue,
+                    new LruBitmapCache());
+        }
+        return this.mImageLoader;
+    }
+ 
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+ 
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+ 
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
 }
